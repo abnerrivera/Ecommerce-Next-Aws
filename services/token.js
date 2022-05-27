d4e5dde83d5476c7c375de04ba2ea5f73392e292
abnerrivera <abner500edrey@gmail.com>
2022-05-27 00:31:54 -0500

    create deleteToken and expiredToken logic

--- services/token.js
+++ services/token.js
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode";
 import { TOKEN } from "../utils/constants";
 
 export function setToken (token){
@@ -6,4 +7,21 @@ export function setToken (token){
 
 export function getToken () {
   return localStorage.getItem(TOKEN)
+}
+
+export function deleteToken() {
+  localStorage.removeItem(TOKEN)
+}
+
+export function hasExpiredToken(token){
+  const tokenDecode = jwtDecode(token)//DECODIFICAMOS EL TOKEN USANDO LA LIBRERIA JWT
+  const expireDate = tokenDecode.exp * 1000; //TOMAMOS LA PROPIEDAD EXP DEL OBJETO Y MULTIPLICAOS POR MIL PAR AUSARLO EN SEGUNDOS
+  const currentDate = new Date().getTime; //TOMAMOS LA FECHA ACTUAL
+  
+  //EXPIRO?
+  if(currentDate > expireDate){
+    return true;
+  }
+    return false;
+  
 }
