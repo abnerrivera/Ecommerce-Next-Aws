d5e44e56df178e63137002010573f7b8beaf8a5c
abnerrivera <abner500edrey@gmail.com>
2022-05-31 20:37:28 -0500

    implement package clasNames

--- layouts/BasicLayout/BasicLayout.js
+++ layouts/BasicLayout/BasicLayout.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import { Container } from 'semantic-ui-react'
-import Header from '../../components/Header/Header'
+import React from 'react';
+import classNames from 'classnames';
+import { Container } from 'semantic-ui-react';
+import Header from '../../components/Header/Header';
 
-const BasicLayout = ({ children }) => {
+const BasicLayout = ({ children, className }) => {
+
 	return (
-		<Container fluid className="basic-layout">
+		<Container fluid
+			className={
+				classNames(
+					"basic-layout", {
+					[className]: className, //si la prop de calassName tiene valor lo toma y lo agrega a la clase
+				}
+				)}>
 
 			{/* HEADER */}
-			<Header/>
+			<Header />
 
 			<Container className="content">
 				{children}
 			</Container>
-			
+
 		</Container>
 	)
 }
